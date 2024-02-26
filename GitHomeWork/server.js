0ca89335aa5f40895616d171e05a1f898e80e16d
RX <19somovalexandr99@bk.ru>
2024-02-27 01:01:09 +0500

    Bruh

---
+++ GitHomeWork/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile(__dirname + "/index.html", (err, data) => {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Internal Server Error');
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    }
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
